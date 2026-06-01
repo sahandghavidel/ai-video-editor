bfb298b325236186ad20a775bc78c0b3d18a5e54
Sahand Ghavidel <sahand.gha@gmail.com>
2026-06-01 14:32:27 +0700

    feat: add experimental webpack memory optimizations in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    webpackMemoryOptimizations: true,
+  },
   outputFileTracingRoot: path.join(__dirname),
 };
 
